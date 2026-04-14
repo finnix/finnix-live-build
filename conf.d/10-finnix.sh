68ded720cdf32fac09a7d1eec5d486b324a6c1fd
Ryan Finnie <ryan@finnie.org>
2026-04-13 22:14:37 -0700

    Document config options, move Finnix-specific to conf.d

---
+++ conf.d/10-finnix.sh
@@ -0,0 +1,12 @@
+# SPDX-PackageName: finnix-live-build
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://github.com/rfinnie/finnix-live-build
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+# shellcheck shell=sh
+
+PRODUCT="${PRODUCT:-Finnix}"
+PRODUCT_ID="${PRODUCT_ID:-finnix}"
+VERSION="${VERSION:-dev}"
+CODENAME="${CODENAME:-Fond du Lac}"
