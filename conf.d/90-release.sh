30f31057096a8105a8204cc7fe6b3158b2aab598
Ryan Finnie <ryan@finnie.org>
2026-08-09 09:46:34 -0700

    Finnix 999 (FAKE, TEST)

---
+++ conf.d/90-release.sh
@@ -0,0 +1,12 @@
+# SPDX-PackageName: finnix-live-build
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://forge.colobox.com/finnix/finnix-live-build
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+
+# shellcheck shell=sh
+# shellcheck disable=SC2034
+
+VERSION="999"
+SAVE_ISO="true"
+CACHE_FROZEN="true"
